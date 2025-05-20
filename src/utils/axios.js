fe16d751b82bd7d18d6b32fae552b5e81e8911ba
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-20 11:46:31 +0530

    added more booking details V1.7

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://shaadisetgo-backend.onrender.com',
+  baseURL: (process.env.REACT_APP_API_URL || 'https://shaadisetgo-backend.onrender.com') + '/api',
   withCredentials: true,
   headers: {
     'Content-Type': 'application/json'
