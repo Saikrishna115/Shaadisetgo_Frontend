96ee9fe820547b3efb22c389be1d80f2821122bf
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-22 13:24:09 +0530

    Final 1.5.2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { Toaster } from 'react-hot-toast';
+import * as serviceWorker from './serviceWorker';
+import store from './store';
 import App from './App';
 import './index.css';
 
@@ -9,7 +12,24 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 3000,
+            style: {
+              background: '#333',
+              color: '#fff',
+            },
+          }}
+        />
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorker.register();
