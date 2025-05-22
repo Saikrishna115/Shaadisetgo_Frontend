8be1c34fac6132a8818ffedb894694b21857df4e
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-22 23:42:20 +0530

    Final 1.5.8

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../context/AuthContext';
 
 // Layouts
 import MainLayout from '../layouts/MainLayout';
