1986345f5f7887e249a72d668c4a757fe4cd1252
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-26 12:31:20 +0530

    corrected regf guy msa

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useMemo } from 'react';
+import React, { useState, } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { register } from '../store/slices/authSlice';
