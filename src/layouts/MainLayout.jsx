83f4200401c3735ede34abfaf829a3e762f68ac5
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-29 21:53:53 +0530

    corrected js config 1

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
-//import Navbar from '../components/ui/Navbar';
+import Header from '@/components/Header';
+import Navbar from '../components/ui/Navbar';
 import Footer from '../components/ui/Footer';
 
 const MainLayout = () => {
   return (
     <div className="min-h-screen flex flex-col">
+      <Header />
       <Navbar />
       <main className="flex-grow">
         <Outlet />
