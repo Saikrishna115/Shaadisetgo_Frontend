99be4016ddc1270d559616a67a3fc17ea45f1e4a
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-05-29 17:21:56 +0530

    corrected home 25

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Header from '../components/Header';
+import Header from '../srccomponents/Header';
 //import Navbar from '../components/ui/Navbar';
 import Footer from '../components/ui/Footer';
 
