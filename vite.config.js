a8c03c8c34847fe347d9d57736be32b435078474
Sai Krishna <ryali.saikrishna1998@gmail.com>
2025-06-03 11:44:06 +0530

    Modified version 1.0.6

--- vite.config.js
+++ vite.config.js
@@ -21,6 +21,7 @@ export default defineConfig({
       input: {
         main: path.resolve(__dirname, 'index.html'),
       },
+      external: ['lucide-react'],
     },
     commonjsOptions: {
       include: [/node_modules/],
